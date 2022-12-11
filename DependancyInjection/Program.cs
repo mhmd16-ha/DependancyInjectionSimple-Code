5d0eaed2c6b2e6edc169d8891392712c57577b62
يارب <@DESKTOP-GHSHA8O>
2022-12-11 20:52:05 +0200

    Add project files.

---
+++ DependancyInjection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DependancyInjection
+{
+    class Mobile
+    {
+        int x;
+        int y;
+        public Mobile(int _x,int _y)
+        {
+            this.x = _x;
+            this.y = _y;
+
+        }
+    }
+    class Web
+    {
+      private  Mobile r;
+        public Web(Mobile _r)
+        {
+            //Mobile m = new Mobile(10);//erro
+            r = _r;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
